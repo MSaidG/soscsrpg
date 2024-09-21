aa7968d07006e3de23029a0b67d979b0f568ec5a
MSaidG <msgulberk@gmail.com>
2024-09-22 02:15:34 +0300

    Add player data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Engine.ViewModels;
 
 namespace soscsrpg
 {
@@ -16,9 +8,17 @@ namespace soscsrpg
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+            _gameSession = new GameSession();
+            DataContext = _gameSession;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _gameSession.player.ExperiencePoints += 10;
         }
     }
 }
